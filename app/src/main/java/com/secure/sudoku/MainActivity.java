7114efcd464d6c60237f9463d7a328e4b185222f
SatyamSoni23 <2018kucp1121@iiitkota.ac.in>
2020-07-29 22:49:30 +0530

    Initial commit

---
+++ app/src/main/java/com/secure/sudoku/MainActivity.java
@@ -0,0 +1,26 @@
+package com.secure.sudoku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
