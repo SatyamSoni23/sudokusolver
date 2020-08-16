502dbaec30e00edf080df965a90d88c50042fe76
SatyamSoni23 <2018kucp1121@iiitkota.ac.in>
2020-08-16 20:13:55 +0530

    big

--- puzzle_scanner/src/main/java/com/secure/puzzle_scanner/ColourFilter.java
+++
@@ -1,4 +0,0 @@
-package com.secure.puzzle_scanner;
-
-public class ColourFilter {
-}
--- puzzle_scanner/src/main/java/com/secure/puzzle_scanner/PuzzleNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.secure.puzzle_scanner;
-
-public class PuzzleNotFoundException extends Exception {
-    public PuzzleNotFoundException(String message){
-        super(message);
-    }
-}
